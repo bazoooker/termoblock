393d242582344455791914ca3bb2249eeb9775c0
bazoooker <manakov55@yandex.ru>
2019-06-26 16:49:26 +0700

    deploy version 1.0

--- distrib/js/script.js
+++ distrib/js/script.js
@@ -113,7 +113,21 @@ $(document).ready(function(){
         $('.preloader').addClass('preloader_hidden');
     }, 1000);
     
-    const tilt = $('.js-tilt').tilt();
+    const tilt = $('.js-tilt').tilt({
+        perspective:    200,   // Transform perspective, the lower the more extreme the tilt get
+    });
+    const tiltMap = $('.js-tilt-map').tilt({
+        maxTilt:        10,
+        perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
+        easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+        scale:          1,      // 2 = 200%, 1.5 = 150%, etc..
+        speed:          300,    // Speed of the enter/exit transition.
+        transition:     true,   // Set a transition on enter/exit.
+        disableAxis:    null,   // What axis should be disabled. Can be X or Y.
+        reset:          true,   // If the tilt effect has to be reset on exit.
+        glare:          false,  // Enables glare effect
+        maxGlare:       1       // From 0 - 1.
+    });
 
     $('.content-slide').eq(0).find('.hideme').addClass('hideme_visible');
 
