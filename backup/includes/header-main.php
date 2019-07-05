0c611b8d431218fdb345fabf54b297ae99570ad5
bazoooker <manakov55@yandex.ru>
2019-07-05 17:36:51 +0700

    bitrix template

---
+++ backup/includes/header-main.php
@@ -0,0 +1,89 @@
+
+
+
+
+
+
+    <body class="wrapper">
+
+        <div class="preloader">
+            <div id="cube-loader">
+                <div class="caption">
+                  <div class="cube-loader">
+                    <div class="cube loader-1"></div>
+                    <div class="cube loader-2"></div>
+                    <div class="cube loader-4"></div>
+                    <div class="cube loader-3"></div>
+                  </div>
+                </div>
+              </div>
+        </div>
+            
+
+        <header class="header">
+            <div class="container header__content">
+                <div class="header__menu-btn">
+                    <div class="btn-burger js-open-mobile-menu">
+                        <span class="btn-burger__bar"></span>
+                        <span class="btn-burger__bar"></span>
+                        <span class="btn-burger__bar"></span>
+                    </div>
+                </div>
+                <div class="header__nav">
+                    <div class="nav">
+                        <a href="/" class="nav__link link link_deco link_deco-l">Продукция</a>
+                        <a href="/" class="nav__link link link_deco link_deco-l">Сервис</a>
+                        <a href="/" class="nav__link link link_deco link_deco-l">Металлообработка</a>
+                        <a href="/" class="nav__link link link_deco link_deco-l">Новости</a>
+                        <a href="/" class="nav__link link link_deco link_deco-l">Компания</a>
+                        <a href="/" class="nav__link link link_deco link_deco-l">Контакты</a>
+                    </div>
+                </div>
+                <div class="header__options">
+                    <div class="user-options">
+                        <button class="user-options__city js-city-change">
+                            Ваш город:&nbsp;&nbsp;
+                            <span class="js-city-holder">Москва&nbsp;&nbsp;<i class="fa fa-caret-down" aria-hidden="true"></i></span>
+                            
+                        </button>
+                        <a href="tel:88001003734" class="user-options__tel btn btn_main btn_size-m"><i class="fa fa-phone" aria-hidden="true"></i>&nbsp;&nbsp;8-800-100-37-34</a>
+
+                        <div class="user-options__city-list">
+                            <span class="user-options__city-option js-city-inject">Барнаул</span>
+                            <span class="user-options__city-option js-city-inject">Санкт-Петербург</span>
+                            <span class="user-options__city-option js-city-inject">Краснодар</span>
+                            <span class="user-options__city-option js-city-inject">Воронеж</span>
+                            <span class="user-options__city-option js-city-inject">Нижневартовск</span>
+                            <span class="user-options__city-option js-city-inject">Барнаул</span>
+                            <span class="user-options__city-option js-city-inject">Санкт-Петербург</span>
+                            <span class="user-options__city-option js-city-inject">Краснодар</span>
+                            <span class="user-options__city-option js-city-inject">Воронеж</span>
+                            <span class="user-options__city-option js-city-inject">Нижневартовск</span>
+                            <span class="user-options__city-option js-city-inject">Барнаул</span>
+                            <span class="user-options__city-option js-city-inject">Санкт-Петербург</span>
+                            <span class="user-options__city-option js-city-inject">Краснодар</span>
+                            <span class="user-options__city-option js-city-inject">Воронеж</span>
+                            <span class="user-options__city-option js-city-inject">Нижневартовск</span>
+                            <span class="user-options__city-option js-city-inject">Барнаул</span>
+                            <span class="user-options__city-option js-city-inject">Санкт-Петербург</span>
+                            <span class="user-options__city-option js-city-inject">Краснодар</span>
+                            <span class="user-options__city-option js-city-inject">Воронеж</span>
+                            <span class="user-options__city-option js-city-inject">Нижневартовск</span>
+                            <span class="user-options__city-option js-city-inject">Барнаул</span>
+                            <span class="user-options__city-option js-city-inject">Санкт-Петербург</span>
+                            <span class="user-options__city-option js-city-inject">Краснодар</span>
+                            <span class="user-options__city-option js-city-inject">Воронеж</span>
+                            <span class="user-options__city-option js-city-inject">Нижневартовск</span>
+                            <span class="user-options__city-option js-city-inject">Барнаул</span>
+                            <span class="user-options__city-option js-city-inject">Санкт-Петербург</span>
+                            <span class="user-options__city-option js-city-inject">Краснодар</span>
+                            <span class="user-options__city-option js-city-inject">Воронеж</span>
+                            <span class="user-options__city-option js-city-inject">Нижневартовск</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+
+        <main class="content" role="main">
---
+++ backup/includes/pagetop.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+<html lang="ru">
+
+
+    <head>
+        <!-- initial -->
+        <meta charset="utf-8" />
+        <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        
+        <!-- fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto|Russo+One&display=swap" rel="stylesheet">
+        
+        <!-- favicons -->
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="theme-color" content="#ffffff">
+
+
+        <!-- title -->
+        <title>
+              Термоблок
+        </title>
+
+        <!-- meta -->
+        <meta content="Термоблок" name="description" />
+        <meta content="Термоблок" name="keywords" />
+
+        <!-- styles -->
+        <link href="css/swiper.min.css" rel="stylesheet"/>            
+        <link href="css/main.css" rel="stylesheet"/>
+        <link href="css/custom.css" rel="stylesheet"/>
+
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+    </head>
